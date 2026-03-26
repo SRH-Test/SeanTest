22ad22d10752cb35e1d876e65241d9eb4b78f8fa
Sean Heeley <67065230+sheeley18@users.noreply.github.com>
2026-03-26 13:25:56 -0400

    Add files via upload

--- wiz_code_testing.tf
+++ wiz_code_testing.tf
@@ -35,15 +35,21 @@ resource "aws_vpc" "wiz_code" {
   cidr_block = "10.200.0.0/16"
 }
 
-resource "aws_subnet" "wiz_code" {
+resource "aws_subnet" "wiz_code1" {
   vpc_id            = aws_vpc.wiz_code.id
-  cidr_block        = "10.200.0.0/24"
+  cidr_block        = cidrsubnet(aws_vpc.wiz_code.cidr_block, 8, 0)
   availability_zone = "us-east-1a"
+}
+
+resource "aws_subnet" "wiz_code2" {
+  vpc_id            = aws_vpc.wiz_code.id
+  cidr_block        = cidrsubnet(aws_vpc.wiz_code.cidr_block, 8, 1)
+  availability_zone = "us-east-1b"
 }
 
 resource "aws_network_acl" "wiz_code" {
   vpc_id     = aws_vpc.wiz_code.id
-  subnet_ids = [aws_subnet.wiz_code.id]
+  subnet_ids = [aws_subnet.wiz_code1.id, aws_subnet.wiz_code2.id]
 
   ingress {
     protocol   = "-1"
@@ -62,4 +68,45 @@ resource "aws_network_acl" "wiz_code" {
     from_port  = 0
     to_port    = 0
   }
+}
+
+# RDS DB Subnet Group
+resource "aws_db_subnet_group" "wiz_code" {
+  subnet_ids = [aws_subnet.wiz_code1.id, aws_subnet.wiz_code2.id]
+}
+
+# Security Group for RDS
+resource "aws_security_group" "rds_wiz_code" {
+  vpc_id = aws_vpc.wiz_code.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# RDS MySQL Instance
+resource "aws_db_instance" "wiz_code" {
+  identifier             = "wiz-code-mysql"
+  engine                 = "mysql"
+  engine_version         = "8.0"
+  instance_class         = "db.t3.micro"
+  allocated_storage      = 20
+  db_subnet_group_name   = aws_db_subnet_group.wiz_code.name
+  vpc_security_group_ids = [aws_security_group.rds_wiz_code.id]
+  publicly_accessible    = true
+  skip_final_snapshot    = true
+
+  db_name  = "wiz_code_test_db"
+  username = "admin"
+  password = "admin"
 }
