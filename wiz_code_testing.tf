10c18ce53c2c08083cab32112f0d65e0b8c233ba
Sean Heeley <67065230+sheeley18@users.noreply.github.com>
2026-03-26 13:28:17 -0400

    Add files via upload

--- wiz_code_testing.tf
+++ wiz_code_testing.tf
@@ -1,34 +1,34 @@
 # This is all bad stuff that one should not do
 
-# Public S3 bucket
-resource "aws_s3_bucket" "public_bucket" {
-  bucket = "fabien-wiz-code-public-bucket"
-}
+# # Public S3 bucket
+# resource "aws_s3_bucket" "public_bucket" {
+#   bucket = "fabien-wiz-code-public-bucket"
+# }
 
-resource "aws_s3_bucket_public_access_block" "public_bucket" {
-  bucket = aws_s3_bucket.public_bucket.id
+# resource "aws_s3_bucket_public_access_block" "public_bucket" {
+#   bucket = aws_s3_bucket.public_bucket.id
 
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
+#   block_public_acls       = false
+#   block_public_policy     = false
+#   ignore_public_acls      = false
+#   restrict_public_buckets = false
+# }
 
-resource "aws_s3_bucket_policy" "public_bucket" {
-  bucket = aws_s3_bucket.public_bucket.id
+# resource "aws_s3_bucket_policy" "public_bucket" {
+#   bucket = aws_s3_bucket.public_bucket.id
 
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect    = "Allow"
-        Principal = "*"
-        Action    = "s3:GetObject"
-        Resource  = "${aws_s3_bucket.public_bucket.arn}/*"
-      }
-    ]
-  })
-}
+#   policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Effect    = "Allow"
+#         Principal = "*"
+#         Action    = "s3:GetObject"
+#         Resource  = "${aws_s3_bucket.public_bucket.arn}/*"
+#       }
+#     ]
+#   })
+# }
 
 # VPC
 resource "aws_vpc" "wiz_code" {
